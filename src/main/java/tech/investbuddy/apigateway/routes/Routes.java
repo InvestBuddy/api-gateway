1f00d6d067dc97e3f4d0b4b82e9fccbc9e3d5b0a
Boubacar Sangare <boubacar34sangare@gmail.com>
2024-12-26 18:00:51 +0100

    adding webconfig for api calls

---
+++ src/main/java/tech/investbuddy/apigateway/routes/Routes.java
@@ -0,0 +1,42 @@
+package tech.investbuddy.apigateway.routes;
+
+import org.apache.catalina.filters.RequestFilter;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceRoute(){
+        return GatewayRouterFunctions.route("user-service")
+                .route(RequestPredicates.path("/api/v1/users/**"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userProfileServiceRoute(){
+        return GatewayRouterFunctions.route("user-profile-service")
+                .route(RequestPredicates.path("/api/v1/profiles/**"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> kycServiceRoute(){
+        return GatewayRouterFunctions.route("kyc-service")
+                .route(RequestPredicates.path("/api/v1/kyc/**"), HandlerFunctions.http("http://localhost:8084"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> discoveryServerRoute() {
+        return GatewayRouterFunctions.route("discovery-server")
+                .route(RequestPredicates.path("/"), HandlerFunctions.http("http://localhost:8761"))
+                .build();
+    }
+}
