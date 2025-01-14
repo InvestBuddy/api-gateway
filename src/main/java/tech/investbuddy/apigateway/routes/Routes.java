971f6033ed8ae7270e0a38370d94873a7d5e2ae1
Boubacar Sangare <boubacar34sangare@gmail.com>
2025-01-14 20:04:30 +0100

    adding prediction route

--- src/main/java/tech/investbuddy/apigateway/routes/Routes.java
+++ src/main/java/tech/investbuddy/apigateway/routes/Routes.java
@@ -34,6 +34,13 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> PredictionServiceRoute() {
+        return GatewayRouterFunctions.route("prediction-service")
+                .route(RequestPredicates.path("/api/v1/prediction/**"), HandlerFunctions.http("http://localhost:8085"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> discoveryServerRoute() {
         return GatewayRouterFunctions.route("discovery-server")
                 .route(RequestPredicates.path("/"), HandlerFunctions.http("http://localhost:8761"))
